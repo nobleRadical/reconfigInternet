43bcb2cbc7f43da8e939fbc86ede387f6b673035
nobleRadical <88314680+nobleRadical@users.noreply.github.com>
2022-05-16 14:00:45 -0700

    rewrite

--- ReconfigureInternet.py
+++ ReconfigureInternet.py
@@ -3,24 +3,28 @@
 # version 0.0.1
 
 import subprocess
+import re
 
 # TODO get SSID, password from file
 ssid = "2.4 Managment"
 pwd = "Nimrod123"
 
-# write to config file: /home/pi/wpaConfig.conf
-configHandle = open("/home/pi/wpaConfig.conf", 'w')
-configString = """ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
-update_config=1
-country=US
+# TODO add network to supplicant
+# check if network already exists in list first; get network ID
+regex = f'(\d)       {ssid}'
+networkList = subprocess.run("wpa_cli -i wlan0 list_networks", shell=True, check=True, capture_output=True, text=True)
+matches = re.findall(regex, networkList.stdout)
+if matches.len <= 0: # no matches
+    # TODO create network
+    addNetwork = subprocess.run(['wpa_cli', '-iwlan0', 'add_network'], shell=True, check=True, capture_output=True, text=True)
+    networkID = addNetwork.stdout
+    setNetworkSSID = subprocess.run(['wpa_cli', '-iwlan0', 'set_network', networkID, 'ssid', ssid], shell=True, check=True)
+    setNetworkPwd = subprocess.run(['wpa_cli', '-iwlan0', 'set_network', networkID, 'psk', pwd], shell=True, check=True)
+else:
+    # make that network id the one to select.
+    networkID = matches[0]
 
-network={
-        ssid="%s"
-        psk="%s"
-}""" % (ssid,pwd)
-configHandle.write(configString)
-configHandle.close()
 
-# run bash commands
+# TODO select network
 
-subprocess.run("/home/pi/riGit/internal.sh", check=True)
+selectNetwork = subprocess.run(['wpa_cli', '-iwlan0', 'select_network', networkID], shell=True, check=True)
