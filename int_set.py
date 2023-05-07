0fcd2a54b31a06d9da8b5444230f980fa0f86bea
nobleRadical <88314680+nobleRadical@users.noreply.github.com>
2023-05-06 21:38:39 -0700

    Final Touches: remove debug code

--- int_set.py
+++ int_set.py
@@ -16,11 +16,7 @@ def strToHex(string):
 
 def connect(interface, network_id, ssid, password):
     ssid_hex = strToHex(ssid)
-    setNetworkSSID = subprocess.run(f'sudo /usr/sbin/wpa_cli -i{interface} set_network {network_id} ssid "{ssid_hex}"', shell=True, check=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if setNetworkSSID.returncode != 0:
-        print(setNetworkSSID.stdout)
-        print("____________________")
-        print(setNetworkSSID.stderr)
+    setNetworkSSID = subprocess.run(f'sudo /usr/sbin/wpa_cli -i{interface} set_network {network_id} ssid "{ssid_hex}"', shell=True, check=True)
     if password:
         setNetworkPwd = subprocess.run(f'sudo /usr/sbin/wpa_cli -i{interface} set_network {network_id} psk \'"{password}"\'', shell=True, check=True)
     setNetworkSSID = subprocess.run(f'sudo /usr/sbin/wpa_cli -i{interface} enable_network {network_id}"', shell=True, check=True)
